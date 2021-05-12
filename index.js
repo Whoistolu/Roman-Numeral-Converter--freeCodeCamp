62124700c82276dadf3ffe9eb872755025b4d136
Ajise <digittolu25@gmail.com>
2021-05-12 17:06:45 +0100

    add solution to challenge

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function convertToRoman(num) {
-    return num;
+    let romanToNum = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    
+    let roman = "";
+
+    for (var key in romanToNum) {
+        while (num >= romanToNum[key]) {
+            roman += key;
+            num -= romanToNum[key];
+        }
+    }
+
+    return roman;
    }
    
    convertToRoman(36);
